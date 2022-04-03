762db6f06fbfbd07a9580eb698145152bcbf5501
kaanboyacii <kaanboyacibn@gmail.com>
2022-04-03 15:38:41 +0300

    admin_logincheck repair

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,25 +17,21 @@ class HomeController extends Controller
     public function login(){
         return view(view:'admin.login');
     }
-    public function logincheck(Request $request){
-        if($request->isMethod('post'))
-        {
-            $credentials = $request->only('email','password');
-            if(Auth::attempt($credentials)){
-                $request->session()->regenerate();
-                return redirect()->intended('admin');
-            }
-            return back()->withErrors([
-                'email' => 'The provided credentials do not match our records.',
-            ]);
-        }
-        else
-        {
-            return view('admin.login');
+    public function logincheck(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('admin');
         }
 
-    // public function test($id){
-    //     echo "Id Number:",$id;
-    // }
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput(['email']);
     }
 }

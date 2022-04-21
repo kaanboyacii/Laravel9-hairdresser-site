d5b6985313679c914f9f5269805d40ea20419db6
kaanboyacii <kaanboyacibn@gmail.com>
2022-04-21 19:06:36 +0300

    dynamic slider on the homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,26 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\service;
+
 
 class HomeController extends Controller
 {
-    //
-    public function index(){
-        return view('home.index');
+
+    public function index()
+    {
+        $sliderdata = Service::limit(4)->get();
+        return view('home.index',compact('sliderdata')
+        );
     }
-    public function aboutus(){
-        return view(view:'home.about');
+    public function aboutus()
+    {
+        return view(view: 'home.about');
     }
-    public function login(){
-        return view(view:'admin.login');
+    public function login()
+    {
+        return view(view: 'admin.login');
     }
     public function logincheck(Request $request)
     {
-        if ($request->isMethod('post'))
-        {
-            $credentials = $request->only('email','password');
-            if (Auth::attempt($credentials))
-            {
+        if ($request->isMethod('post')) {
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
 
 
@@ -32,14 +39,12 @@ class HomeController extends Controller
             return back()->withErrors([
                 'email' => 'The provided credentials do not match our'
             ]);
-        }
-        else
-        {
+        } else {
             return view('admin.login');
         }
-
     }
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
--- resources/views/home/_home.blade.php
+++ resources/views/home/_home.blade.php
@@ -2,66 +2,48 @@
 
 	<div class="home">
 
-		<!-- Home Slider -->
+	    <!-- Home Slider -->
 
-		<div class="home_slider_container">
-			<div class="owl-carousel owl-theme home_slider">
+	    <div class="home_slider_container">
+	        <div class="owl-carousel owl-theme home_slider">
 
-				<!-- Home Slider Item -->
-				<div class="owl-item">
-					<div class="home_slider_background" style="background-image:url({{asset('assets')}}/images/home_slider_1.jpg)"></div>
-					<div class="home_slider_content">
-						<div class="home_slider_content_inner">
-							<div class="home_slider_subtitle">Promo Prices</div>
-							<div class="home_slider_title">New Collection</div>
-						</div>
-					</div>
-				</div>
+	            <!-- Home Slider Item -->
+	            @foreach($sliderdata as $rs)
+	            <div class="owl-item">
+	                <div class="home_slider_background" style="background-image: url('{{ Storage::url("{$rs->image}") }}');"></div>
+	                <div class="home_slider_content">
+	                    <div class="home_slider_content_inner">
+	                        <h1 style="text-align:center;color:white;">{{$rs->title}}</h1>
+	                        <div class="home_slider_subtitle">Promo Prices</div>
+	                        <div class="home_slider_title">New Services</div>
+	                    </div>
+	                </div>
+	            </div>
+	            @endforeach
 
-				<!-- Home Slider Item -->
-				<div class="owl-item">
-					<div class="home_slider_background" style="background-image:url({{asset('assets')}}/images/home_slider_1.jpg)"></div>
-					<div class="home_slider_content">
-						<div class="home_slider_content_inner">
-							<div class="home_slider_subtitle">Promo Prices</div>
-							<div class="home_slider_title">New Collection</div>
-						</div>
-					</div>
-				</div>
+	        <!-- Home Slider Nav -->
 
-				<!-- Home Slider Item -->
-				<div class="owl-item">
-					<div class="home_slider_background" style="background-image:url({{asset('assets')}}/images/home_slider_1.jpg)"></div>
-					<div class="home_slider_content">
-						<div class="home_slider_content_inner">
-							<div class="home_slider_subtitle">Promo Prices</div>
-							<div class="home_slider_title">New Collection</div>
-						</div>
-					</div>
-				</div>
+	        <div class="home_slider_next d-flex flex-column align-items-center justify-content-center"><img src="{{asset('assets')}}/images/arrow_r.png" alt=""></div>
 
-			</div>
+	        <!-- Home Slider Dots -->
 
-			<!-- Home Slider Nav -->
-
-			<div class="home_slider_next d-flex flex-column align-items-center justify-content-center"><img src="{{asset('assets')}}/images/arrow_r.png" alt=""></div>
-
-			<!-- Home Slider Dots -->
-
-			<div class="home_slider_dots_container">
-				<div class="container">
-					<div class="row">
-						<div class="col">
-							<div class="home_slider_dots">
-								<ul id="home_slider_custom_dots" class="home_slider_custom_dots">
-									<li class="home_slider_custom_dot active">01.<div></div></li>
-									<li class="home_slider_custom_dot">02.<div></div></li>
-									<li class="home_slider_custom_dot">03.<div></div></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	        <div class="home_slider_dots_container">
+	            <div class="container">
+	                <div class="row">
+	                    <div class="col">
+	                        <div class="home_slider_dots">
+	                            <ul id="home_slider_custom_dots" class="home_slider_custom_dots">
+	                                <li class="home_slider_custom_dot active">01.<div></div>
+	                                </li>
+	                                <li class="home_slider_custom_dot">02.<div></div>
+	                                </li>
+	                                <li class="home_slider_custom_dot">03.<div></div>
+	                                </li>
+	                            </ul>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
 	</div>

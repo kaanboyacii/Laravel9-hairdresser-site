93c834476926ff9e0b00e4a3943b98a8299169c8
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-05-18 18:24:52 +0300

    User permissions check admin user for admin panel

--- routes/web.php
+++ routes/web.php
@@ -37,16 +37,18 @@ Route::get('/contact',[HomeController::class, 'contact'])->name('contact');
 Route::post('/storemessage',[HomeController::class, 'storemessage'])->name('storemessage');
 Route::get('/faq',[HomeController::class, 'faq'])->name('faq');
 Route::post('/storecomment',[HomeController::class, 'storecomment'])->name('storecomment');
-Route::view('/loginuser','home.login');
-Route::view('/registeruser','home.register');
+Route::view('/loginuser','home.login')->name('loginuser');
+Route::view('/registeruser','home.register')->name('registeruser');
 Route::get('/logoutuser',[HomeController::class, 'logout'])->name('logoutuser');
+Route::view('/loginadmin','admin.login')->name('loginadmin');
+Route::post('/loginadmincheck',[HomeController::class, 'loginadmincheck'])->name('loginadmincheck');
 
 
 Route::get('/service/{id}',[HomeController::class, 'service'])->name('service');
 Route::get('/categoryservices/{id}',[HomeController::class, 'categoryservices'])->name('categoryservices');
 
 //ADMIN PANEL ROUTES
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function() {
+Route::middleware('admin')->prefix('admin')->name('admin.')->group(function() {
     Route::get('/', [AdminHomeController::class, 'index'])->name('index');
     //ADMIN GENERAL ROUTES
     Route::get('/setting', [AdminHomeController::class, 'setting'])->name('setting');
